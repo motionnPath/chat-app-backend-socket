c880bef4124687726d94edf6d4967fac1359a3ec
motionnPath <mathlouthi.91.oussama@gmail.com>
2023-11-09 21:27:30 +0100

    updated cors for io

--- soket.js
+++ soket.js
@@ -26,7 +26,9 @@ const server = http.createServer(app);
 const io = new Server(server,{
     cors:{
         origin: process.env.CLIENT_URL, 
-        methods: ['GET', 'POST']
+        methods: ['GET', 'POST'],
+        allowedHeaders:['test'],
+        credentials:true
     }
 });
 
