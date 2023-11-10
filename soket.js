a66bc3260989e04c7f9690f146dcbeb03be5a2fc
motionnPath <mathlouthi.91.oussama@gmail.com>
2023-11-10 01:42:08 +0100

    it worked just after this commit

--- soket.js
+++ soket.js
@@ -28,13 +28,89 @@ router.get("/", (req, res) => {
   res.send({ response: "Server is up and running." }).status(200);
 });
 
+
 let private_room
 let onlineUsers = []; // Store user information and socket connections
 
 io.on('connection', (socket) => {
 
-    console.log('soket ir ===', socket.id)
+  console.log("socket id =",socket.id)
+  
+  socket.on("join-room",({room})=> {
+    private_room = room
+  })
 
+  // asign userName to its soketID
+  socket.on('startChat', ({username}) => {
+      // if u have no such username assign a soketId to that unique username
+    if (!onlineUsers.some(user => user.username === username)) {
+      onlineUsers.push(({
+        username,
+        socketId: socket.id,
+      }))
+    }else {
+      // if u do have such a username pls update its socketId 
+      const index = onlineUsers.findIndex(user => user.username === username);
+      onlineUsers[index].socketId = socket.id;
+      
+    }
+    
+    console.log("OnlineUsers",onlineUsers)
+  });
+
+  // Handle private messages
+  socket.on('privateMessage', async (data) => {
+    const { from,to, message,room_1,room_2} = data;
+
+    socket.join(room_1)
+    socket.join(room_2)
+    //console.log("data = ",data)
+    let reciptient = onlineUsers.find(u => u.username === to.toString())
+    let sender = onlineUsers.find(u => u.username === from.toString())
+   
+   
+    // if recipient is connected !
+    if(reciptient) {
+      io.to([room_1,room_2])
+        .to([reciptient.socketId,sender.socketId])
+        .emit('getPrivateMessage',{
+          message,
+          from:sender.username,
+          time: (new Date()), 
+          room:private_room
+        })
+    }else { // if not just send it to the sender and deliver the msg to mongoDB in line 85
+      
+      io.to([sender.socketId])
+        .emit('getPrivateMessage',{
+          message,
+          from:sender.username,
+          time: (new Date()), 
+          room:private_room
+        })
+    }
+    
+    
+    // saving msg data to database
+    if(data){
+      axios.post(`${server_url}/conversations/new-conversation`,{
+        conversation: {
+          message,
+          from:sender.username,
+          time: (new Date()),
+          room:private_room
+        }
+      }).then( d => {
+        //console.log('new conversation added',d)
+      }).catch(e => console.error(' Error creating soket conversation',e))
+    }
+    
+  });
+
+  socket.on('disconnect', () => {
+    
+   
+  });
 });
 
 server.listen(process.env.PORT, ()=>{
